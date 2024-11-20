f3c37fb4612c67b084dfcf54096c7c5b9691af46
root <nayeeman49@googlemail.com>
2024-11-20 22:25:39 +0000

    first commit

---
+++ variable.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "hello world"
+
+echo our shell name is $BASH
+echo our shell version name is $BASH_VERSION
+echo our home directory is $HOME
+echo current working directory is $PWD
+
+name=mark
+value=10
+echo the NAME is $name
+echo value is $value
+
+
